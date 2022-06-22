bf91aa846a71edaf48581bf4799a1834c0072352
srathod406 <91242840+srathod406@users.noreply.github.com>
2022-06-22 10:57:47 +0530

    hosted

--- router.js
+++ router.js
@@ -1,8 +1,11 @@
 var express=require('express');
 var router=express.Router();
+var Movie=require('./Models/Movie')
 
-router.get("/",(req,res)=>{
-    res.sendFile("index.html",{root:__dirname});
+router.get("/movies",async(req,res)=>{
+    const iMovie=await
+    Movie.find();
+    res.send(iMovie);
 });
 
 module.exports=router
