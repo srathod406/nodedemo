73fa13f3f4f51cefe0837ee1c639e6c1f89a4128
srathod406 <91242840+srathod406@users.noreply.github.com>
2022-06-22 10:42:28 +0530

    hosted

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ var express=require('express')
 var mongoose=require('mongoose')
 var bodyParser=require('body-parser')
 var router= require('./router')
-
+mongoose.connect("mongodb://localhost:27017/cie2").then(()=>{console.log("DB Connected")
 
 var app=express();
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.json())
 app.use('/api',router);
 app.listen(process.env.PORT,()=>{
-    console.log("Srerver Started");
+    console.log("Srerver Started at 5000");
 })
+
+}).catch((err)=>{
+    console.log(err);
+})
+
+
