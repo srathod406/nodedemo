d3290c1a26e601c19f57412e0db964bc63720641
srathod406 <91242840+srathod406@users.noreply.github.com>
2022-06-22 11:38:47 +0530

    hosted

--- Models/Movie.js
+++ Models/Movie.js
@@ -1,4 +1,5 @@
 var mongoose=require("mongoose");
+
 var m=mongoose.Schema({
     name:String,
     rating:Number
--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var express=require('express')
 var mongoose=require('mongoose')
 var bodyParser=require('body-parser')
 var router= require('./router')
-mongoose.connect("mongodb://localhost:27017/cie2").then(()=>{console.log("DB Connected")
+mongoose.connect("mongodb+srv://shrutirathod:mongo123@cluster0.xumhw.mongodb.net/Movie?retryWrites=true&w=majority").then(()=>{console.log("DB Connected")
 
 var app=express();
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.json())
 app.use('/api',router);
+
 app.listen(process.env.PORT,()=>{
     console.log("Srerver Started at 5000");
 })
